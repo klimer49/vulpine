da171330f82de0bb448dff2f1e38d3f59832e3f5
Aleksey Demchenkov <me@pelfox.dev>
2021-12-07 08:46:12 +0300

    Update activate.php

--- accounts/activate.php
+++ accounts/activate.php
@@ -1,5 +1,3 @@
-
-<!-- 0x46e7b0311B7FB95fd9857BDF01d2279D8B9C46d5 -->
 <?php
 include "../libs/main.php";
 include "../config/register.php";
@@ -9,23 +7,23 @@ if($captcha == false) {
     $sql = "SELECT * FROM accounts WHERE registerToken = '$token'"; 
     $result = mysqli_query($link, $sql);
     $row = $result->fetch_assoc();
-    if(!$row["id"]) exit("Activation token not found");
+    if(!$row["id"]) exit("No activation token was found.");
     $sql = "UPDATE accounts SET isActivated = 1 WHERE registerToken = '$token'"; 
     $result = mysqli_query($link, $sql);
-    if(!$result) exit("Error");
-    exit("Activated");
+    if(!$result) exit("Unknown error.");
+    exit("Successful activation!");
 }
 
 $token = htmlspecialchars($_GET["token"]);
 $sql = "SELECT * FROM accounts WHERE registerToken = '$token'"; 
 $result = mysqli_query($link, $sql);
 $row = $result->fetch_assoc();
-if(!$row["id"]) exit("Activation token not found");
+if(!$row["id"]) exit("No activation token was found.");
 if($row["isActivated"] == 1) {
-    exit("You aleready activated your account");
+    exit("You already activated your account.");
 }
 if($_POST) {
-    if(!$_POST["captcha_token"]) exit("Error");
+    if(!$_POST["captcha_token"]) exit("A captcha error has occurred.");
     $data = array(
         'secret' => $captcha_secret,
         'response' => $_POST['h-captcha-response']
@@ -40,8 +38,8 @@ if($_POST) {
     if($responseData->success) {
         $sql = "UPDATE accounts SET isActivated = 1 WHERE registerToken = '".htmlspecialchars($_POST["captcha_token"])."'"; 
         $result = mysqli_query($link, $sql);
-        if(!$result) exit("Error");
-        exit("Activated");
+        if(!$result) exit("Unknown error.");
+        exit("Successful activation!");
     }
 }
 ?>
@@ -90,60 +88,24 @@ if($_POST) {
               cursor: pointer;
             }
 
-            /* .services__card #line span {
-              position: absolute;
-              top: -10px;
-              left: -10px;
-              width: 150px;
-              height: 150px;
-              background: red;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-            }
-
-            .services__card #line span::before {
-              content: '';
-              position: absolute;
-              width: 100%;
-              height: 40px;
-              background: blue;
-            } */
-
             .main {
               background: #303339;
               border-top: 3px solid #525965;
               border-radius: 100px 100px 10px 10px;
-              /* margin-top: 100px; */
-              /* padding: 20px; */
               align-items: center;
               justify-content: center;
               text-align: center;
               display: flex;
               flex-direction: column;
               justify-content: center;
-
               min-height: 100%;
             }
-            /* .logo {
-              background: -webkit-linear-gradient(to right #00DEFF, #4D90FF);
-              background: linear-gradient(to right, #00DEFF, #4D90FF);
-              background-size: 100%;
-              -webkit-background-clip: text;
-              -moz-background-clip: text;
-              -webkit-text-fill-color: transparent;
-              -mo-text-fill-color: transparent;
-            } */
-            /* .title {
-                text-align: center;
-                font-size: 3em;
-                margin-top: 0px;
-                margin-bottom: 100px;
-            } */
+            
             .logo {
               font-size: 18px;
               color: #ffffff52;
             }
+            
             .services__card h1 span {
                 word-break: break-all;
                 background: -webkit-linear-gradient(to right #00DEFF, #4D90FF);
@@ -154,9 +116,11 @@ if($_POST) {
                 -webkit-text-fill-color: transparent;
                 -mo-text-fill-color: transparent;
             }
+            
             .inputblock {
               margin-top: 40px;
             }
+            
             input {
                 background-color: #303339;
                 border: 1px solid #00000000;
@@ -167,6 +131,7 @@ if($_POST) {
                 font-size: 24px;
                 border-radius: 3px;
             }
+            
             input:hover {
                 background-color: #3A3E45;
                 border: 1px solid #00000000;
@@ -175,6 +140,7 @@ if($_POST) {
                 padding: 3px;
                 border-radius: 3px;
             }
+            
             input:focus {
                 background-color: #303339;
                 border: 1px solid #494D55ff;
@@ -184,21 +150,27 @@ if($_POST) {
                 border-radius: 5px;
                 transition: all 0.3s ease;
             }
+            
             .center {
                 text-align: center;
             }
+            
             #status {
                 color: #FF7575;
             }
+            
             #status2 {
                 color: #FF7575;
             }
+            
             .input .error {
               border-left: 2px solid #FF7575;
             }
+            
             .error {
                 color: #FF7575;
             }
+            
             .button {
                 padding: 8px 30px;
                 border-radius: 10px;
@@ -206,23 +178,27 @@ if($_POST) {
                 background-color: #3ba55d;
                 transition: all 0.3s ease;
             }
+            
             .button:hover {
                 padding: 8px 30px;
                 border-radius: 10px;
                 cursor: pointer;
                 background-color: #4EAF6D;
             }
+            
             .button:focus {
                 padding: 8px 30px;
                 border-radius: 10px;
                 cursor: pointer;
                 background-color: #4EAF6D;
             }
+            
             .lds-dual-ring {
               display: inline-block;
               margin-bottom: -2px;
               margin-left: 10px;
             }
+            
             .lds-dual-ring:after {
               content: " ";
               display: block;
@@ -233,6 +209,7 @@ if($_POST) {
               border-color: #fff transparent #fff transparent;
               animation: lds-dual-ring 1.2s linear infinite;
             }
+            
             @keyframes lds-dual-ring {
               0% {
                 transform: rotate(0deg);
@@ -241,21 +218,24 @@ if($_POST) {
                 transform: rotate(360deg);
               }
             }
+            
             @media only screen and (max-width: 1000px) {
               .main {
                 margin-top: 100px;
               }
+              
               .title {
                 margin-top: 1em;
               }
+                
               .services__wrapper {
                 align-items: center;
                 display: grid;
                 grid-template-columns: 1fr;
                 grid-template-rows: 1fr;
               }
+                
               .services__card {
-                  /* max-width: 70%; */
                   margin: 30px;
                   height: 500px;
                   width: 380px;
@@ -266,16 +246,20 @@ if($_POST) {
                   color: #fff;
                   background: #292b2f;
               }
+                
               p {
                 font-size: 24px;
               }
+                
               input {
                 width: 280px;
               }
+                
               .main {
                   width: 100%;
               }
             }
+            
             @media only screen and (min-width: 1001px) {
               .main {
                   margin-left: 20%;
